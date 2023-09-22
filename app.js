28c0d68d8d7bc409ec67abfeb29ce92742b7d0dd
Yoel Cabaleiro <yoelcabaleiro@gmail.com>
2023-09-22 02:22:42 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  };
+  function excuseGenerator() {
+    let finalExcuse = "";
+    let excuse = [
+      [
+        "My dog ",
+        "My cat ",
+        "My nephew ",
+        "My grandma ",
+        "My father in law ",
+        "Your friend ",
+        "My sisters' boyfriend ",
+        "A colony of ants ",
+        "A very furious rat ",
+        "My alter ego "
+      ],
+      [
+        "destroyed ",
+        "broke ",
+        "smashed ",
+        "incinerated ",
+        "obliterated ",
+        "crushed ",
+        "turned into pieces ",
+        "cursed ",
+        "hexed "
+      ],
+      [
+        "my will ",
+        "my 1k+ hours WoW character ",
+        "my aunt's garden ",
+        "my brother's birds ",
+        "my bitcoin wallet ",
+        "an italian restaurant ",
+        "two flat earthers ",
+        "my uncle's favourite chair "
+      ],
+      [
+        "with their political ideas. Savage...",
+        "with an unasked advice.",
+        "because it was rainy... Terrible.",
+        "........What? Yeah, that's all dude...",
+        "with their superpowers... True story.",
+        "by mistake... You know, sh*t happens."
+      ]
+    ];
+    for (let array of excuse) {
+      finalExcuse += array[Math.floor(Math.random() * array.length)];
+    }
+    return finalExcuse;
+  }
+  
